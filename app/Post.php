dd6751a6e2e6d07c787b4da5e2336bbb567893fc
erik schievenin <erik.schievenin@gmail.com>
2022-06-06 18:52:00 +0200

    exercise semi-complete con sofference

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    protected $fillable = ['title', 'content', 'slug'];
+    protected $fillable = ['title', 'content', 'slug', 'category_id'];
+
+
+    public function category(){
+        return $this->belongTo('App\Category');
+    }
+
+    //take a string as input and return a unique slug version
+    public static function convertToSlug($title){
+        $slugPrefix = Str::slug($title);
+
+        $slug = $slugPrefix;
+        $postFound = Post::where('slug', $slug)->first();
+        $counter = 1;
+        while($postFound){
+            $slug = $slugPrefix . '_' . $counter;
+            $counter++;
+            $postFound = Post::where('slug', $slug)->first();
+        }
+        return $slug;
+    }
 }
