04e9878fdca706b7e3ea66169d4df6acfc355387
erik schievenin <erik.schievenin@gmail.com>
2022-06-06 19:02:43 +0200

    Exercise complete

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -1,54 +1,77 @@
 @extends('layouts.dashboard')
 @section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                {{-- Title card --}}
+                <div class="card">
+                    <div class="card-header">Edit a Post</div>
 
-<div class="container">
-    <div class="row">
-        <div class="col-md-8">
-            {{-- Title card  --}}
-            <div class="card">
-                <div class="card-header">Edit a Post</div>
+                    <div class="card-body">
+                        <form action="{{ route('admin.posts.update', $post) }}" method="POST">
+                            {{-- Token --}}
+                            @csrf
+                            {{-- / Token --}}
 
-                <div class="card-body">
-                <form action="{{route('admin.posts.update', $post)}}" method="POST">
-                    {{-- Token  --}}
-                    @csrf
-                    {{-- / Token  --}}
+                            {{-- title post --}}
+                            @method('PUT')
+                            <div class="form-group">
+                                <label for="title">Title:</label>
+                                <input type="text" name="title" class="form-control @error('title') is-invalid @enderror "
+                                    placeholder="Post's title">
+                                @error('title')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+                            </div>
 
-                    {{-- title post --}}
-                    @method('PUT')
-                    <div class="form-group">
-                        <label for="title">Title:</label>
-                        <input type="text" name="title" class="form-control @error('title') is-invalid @enderror " placeholder="Post's title"
+                            {{-- Category --}}
+                            <div class="form-group">
+                                <label for="title">Category:</label>
+                                <select name="category_id">
+                                    <option value="">--Choose category--</option>
+                                    @foreach ($categories as $category)
+                                        <option value="{{ $category->id }}"
+                                            {{ $category->id == old('category_id', $post->category_id) ? 'selected' : '' }}>
+                                            {{ $category->name }} </option>
+                                    @endforeach
+                                </select>
 
-                        >@error('title')
-                        <div class="invalid-feedback">
-                            {{$message}}
-                        </div>
+                                @error('title')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+                            </div>
+                            {{-- / Category --}}
 
-                        @enderror
-                    </div>
-                      <div class="form-group">
-                        <label for="content">Content:</label>
-                        <textarea type="text" name="content" class="form-control @error('content') is-invalid @enderror" placeholder="Post's content">
+                            <div class="form-group">
+                                <label for="content">Content:</label>
+                                <textarea type="text" name="content" class="form-control @error('content') is-invalid @enderror"
+                                    placeholder="Post's content">
 
                         </textarea>
-                         @error('content')
-                                <div class="invalid-feedback">
-                                    {{$message}}
-                                 </div>
-                        @enderror
-                    </div>
-                    {{--/ content post --}}
+                                @error('content')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+                            </div>
+                            {{-- / title post --}}
 
-                    <div class="form-group">
-                        <input type="submit" class="btn btn-info white" value="Edit Post">
+
+                            {{-- / content post --}}
+
+                            <div class="form-group">
+                                <input type="submit" class="btn btn-info white" value="Edit Post">
+                            </div>
+                        </form>
                     </div>
-                </form>
+                </div>
+                <a href="{{ route('admin.posts.index') }}" class="btn btn-success"> Back</a>
+
             </div>
         </div>
-                <a href="{{route('admin.posts.index')}}" class="btn btn-success"> Back</a>
-
-        </div>
     </div>
-</div>
 @endsection
